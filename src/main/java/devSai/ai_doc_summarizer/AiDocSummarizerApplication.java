7e8efa8fa6fabcd08f4d3cdc163423fbda3c085f
manikanta sai <choppasai5@gmail.com>
2025-04-09 16:46:29 +0530

    initial commit

---
+++ src/main/java/devSai/ai_doc_summarizer/AiDocSummarizerApplication.java
@@ -0,0 +1,13 @@
+package devSai.ai_doc_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devSai/ai_doc_summarizer/AiDocSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package devSai.ai_doc_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
